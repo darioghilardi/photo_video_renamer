bde87e62e97be91a770ccffce2122c8e6501acd4
Dario Ghilardi <darioghilardi@webrain.it>
2021-09-03 18:25:48 +0200

    Fast and fully working first version.

--- renamer.exs
+++ renamer.exs
@@ -6,8 +6,13 @@
 # Notes:
 # Command for MOV files:
 #   > exiftool -CreationDate -ee filepath
+#
 # Command for JPEG files:
 #   > exiftool -DateTimeOriginal -ee filepath
+#
+# Fallback for JPEG files:
+#   > exiftool -FileModifyDate -ee filepath
+#
 # Variant using exiv2:
 #   > {out, _status} = System.cmd("exiv2", ["-g", "Exif.Image.DateTime", "-Pv", filepath])
 
@@ -15,127 +20,128 @@ Mix.install([
   {:timex, "~> 3.0"}
 ])
 
-defmodule Video do
+defmodule Reader do
+  @video_ext ~w(.mov .MOV)
+  @image_ext ~w(.jpg .JPG .jpeg .JPEG .heic .HEIC)
+
   def process(filepath) do
-    IO.puts("Processing videos")
-
     filepath
-    |> read_video_date()
-    |> String.split("\n")
-    |> Enum.map(&String.split(&1, "\t"))
-    # Reject videos without date, will need to rename them manually
-    |> Enum.reject(fn i -> Enum.count(i) == 1 end)
-    |> Enum.map(fn [date, filename] -> %{date: date, filename: filename} end)
-    |> Enum.map(&parse_date(:with_timezone, &1))
-    |> Enum.map(&stringify_date(&1))
-    |> IO.inspect(limit: :infinity)
-  end
+    |> read_exif()
+    |> parse_exif()
+    |> Enum.map(&select_date_field/1)
+    |> Enum.map(&stringify_date/1)
+    |> IO.inspect()
+    |> rename_duplicates(filepath)
 
-  defp read_video_date(filepath) do
-    {output, _status} =
-      System.cmd("/bin/sh", [
-        "-c",
-        "exiftool -T -CreationDate -FileName -ee #{filepath}*{.mov,.MOV}"
-      ])
-
-    output
-  end
-
-  defp parse_date(:with_timezone, %{date: date, filename: filename}) do
-    %{
-      date:
-        date
-        |> String.trim("\n")
-        |> String.split("+")
-        |> hd()
-        |> Timex.parse!("%Y:%m:%d %H:%M:%S", :strftime),
-      filename: filename
-    }
-  end
-
-  defp stringify_date(%{date: date} = file) do
-    file |> Map.put(:date, Timex.format!(date, "%Y%m%d_%H%M%S", :strftime))
-  end
-end
-
-defmodule Image do
-  def process(filepath) do
-    IO.puts("Processing images")
-
-    filepath
-    |> read_image_original_date()
-    |> String.split("\n")
-    |> IO.inspect(limit: :infinity)
-
+    # |> String.split("\n")
     # |> Enum.map(&String.split(&1, "\t"))
     # # Reject videos without date, will need to rename them manually
     # |> Enum.reject(fn i -> Enum.count(i) == 1 end)
     # |> Enum.map(fn [date, filename] -> %{date: date, filename: filename} end)
     # |> Enum.map(&parse_date(:with_timezone, &1))
-    # |> IO.inspect(limit: :infinity)
     # |> Enum.map(&stringify_date(&1))
     # |> IO.inspect(limit: :infinity)
   end
 
-  defp read_image_original_date(filepath) do
+  defp read_exif(filepath) do
+    IO.puts("Reading Exif Tags")
+
     {output, _status} =
-      System.cmd("/bin/sh", [
-        "-c",
-        "exiftool -T -DateTimeOriginal -FileName -ee #{filepath}*{.jpg,.JPG,.jpeg,.JPEG,.heic,.HEIC}"
+      System.cmd("exiftool", [
+        "-T",
+        "-CreationDate",
+        "-DateTimeOriginal",
+        "-FileModifyDate",
+        "-FileName",
+        "-ee",
+        filepath
       ])
 
     output
   end
 
-  defp parse_date(:with_timezone, %{date: date, filename: filename}) do
-    %{
-      date:
-        date
-        |> String.trim("\n")
-        |> String.split("+")
-        |> hd()
-        |> Timex.parse!("%Y:%m:%d %H:%M:%S", :strftime),
-      filename: filename
-    }
+  defp parse_exif(output) do
+    IO.puts("Parsing Exif Tags")
+
+    output
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, "\t"))
+    # Reject empty results
+    |> Enum.reject(fn i -> Enum.count(i) == 1 end)
+    |> Enum.map(fn [creation_date, date_time_original, modify_date, filename] ->
+      %{
+        creation_date: parse_date_with_tz(creation_date),
+        date_time_original: parse_date(date_time_original),
+        modify_date: parse_date_with_tz(modify_date),
+        filename: filename
+      }
+    end)
   end
 
-  # defp read_date({:image, filepath}) do
-  #   case read_date_time_original(filepath) do
-  #     "-\n" ->
-  #       IO.inspect("no original date, looking for datetime")
+  defp parse_date("-"), do: "-"
+  defp parse_date(date), do: date |> Timex.parse!("%Y:%m:%d %H:%M:%S", :strftime)
 
-  #       date =
-  #         filepath
-  #         |> read_date_time()
+  defp parse_date_with_tz("-"), do: "-"
 
-  #       {:with_timezone, date}
-
-  #     date ->
-  #       {:no_timezone, date}
-  #   end
-  # end
-
-  defp read_date_time(filepath) do
-    {date, _status} = System.cmd("exiftool", ["-T", "-FileModifyDate", "-ee", filepath])
+  defp parse_date_with_tz(date) do
     date
-  end
-
-  defp parse_date({:no_timezone, date}) do
-    date
-    |> String.trim("\n")
+    |> String.split("+")
+    |> hd()
     |> Timex.parse!("%Y:%m:%d %H:%M:%S", :strftime)
   end
+
+  def select_date_field(%{
+        creation_date: creation_date,
+        date_time_original: date_time_original,
+        modify_date: modify_date,
+        filename: filename
+      }) do
+    ext = Path.extname(filename)
+
+    cond do
+      Enum.member?(@video_ext, ext) ->
+        %{filename: filename, date: creation_date}
+
+      Enum.member?(@video_ext, ext) ->
+        %{filename: filename, date: creation_date}
+
+      Enum.member?(@image_ext, ext) and Timex.is_valid?(date_time_original) ->
+        %{filename: filename, date: date_time_original}
+
+      Enum.member?(@image_ext, ext) and !Timex.is_valid?(date_time_original) ->
+        %{filename: filename, date: modify_date}
+    end
+  end
+
+  defp stringify_date(%{date: date} = file) do
+    file |> Map.put(:date, Timex.format!(date, "%Y%m%d_%H%M%S", :strftime))
+  end
+
+  defp rename_duplicates(list, filepath) do
+    list
+    |> Enum.map(&rename_file(&1, filepath))
+  end
+
+  def rename_file(%{date: date, filename: filename} = file, filepath, suffix \\ 0) do
+    ext = Path.extname(filename)
+    old_name = filepath <> filename
+    new_name = generate_new_name(filepath, date, ext, suffix)
+
+    case(File.exists?(new_name)) do
+      true ->
+        rename_file(file, filepath, suffix + 1)
+
+      false ->
+        File.rename(old_name, new_name)
+        IO.puts("File #{old_name} renamed to #{new_name}")
+    end
+  end
+
+  defp generate_new_name(filepath, date, ext, 0), do: filepath <> date <> ext
+
+  defp generate_new_name(filepath, date, ext, suffix),
+    do: filepath <> date <> "_" <> Integer.to_string(suffix) <> ext
 end
 
 path = System.argv() |> hd()
-
-# return all the dates
-# videos = Video.process(path)
-images = Image.process(path)
-
-# |> File.ls!()
-# |> Enum.map(&Runner.run(path <> &1))
-
-# list the duplicates
-# duplicates = Enum.uniq(dates -- Enum.uniq(dates))
-# IO.inspect(duplicates, label: "duplicates")
+Reader.process(path)
